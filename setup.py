d74d4b3356e632441b49173ebf983a94bdcc492e
Mark Veltzer <mark.veltzer@gmail.com>
2017-04-20 15:44:26 +0300

    reduce config

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if not sys.version_info[0] == 3:
 
 setuptools.setup(
     name='pymount',
-    version='0.0.6',
+    version='0.0.7',
     description='module to help you mount and unmount file systems',
     long_description='programmatic API to /proc/partitions and /proc/mounts',
     url='https://veltzer.github.io/pymount',
