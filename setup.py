8d775faba9f19465c9a48dd7433e6c066c7ab403
Mark Veltzer <mark.veltzer@gmail.com>
2017-07-29 08:42:35 +0300

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if not sys.version_info[0] == 3:
 
 setuptools.setup(
     name='pymount',
-    version='0.0.7',
+    version='0.0.8',
     description='module to help you mount and unmount file systems',
     long_description='programmatic API to /proc/partitions and /proc/mounts',
     url='https://veltzer.github.io/pymount',
