cfb834db8904e63fa58822f254ec52135e2465f6
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-27 19:52:46 +0200

    webpage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.8',
     description='module to help you mount and unmount file systems',
     long_description='programmatic API to /proc/partitions and /proc/mounts',
-    url='https://github.com/veltzer/pymount',
+    url='https://veltzer.github.io/pymount',
     download_url='https://github.com/veltzer/pymount',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
