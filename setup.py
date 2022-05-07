38b458a7bab2f0aff529e991f36300b64ffb015b
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 04:04:19 +0300

    more stuff

--- setup.py
+++ setup.py
@@ -33,10 +33,6 @@ setuptools.setup(
     platforms=[
         'python3',
     ],
-    install_requires=[
-    ],
-    extras_require={
-    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -50,7 +46,5 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={"console_scripts": [
-    ]},
     python_requires=">=3.9",
 )
