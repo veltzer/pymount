11a00d21b02934acdb202525891c94786e2af67c
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 15:04:17 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -4,8 +4,10 @@ config_requires: list[str] = [
     "pyclassifiers",
 ]
 build_requires: list[str] = [
+    "hatch",
     "pydmt",
     "pymakehelper",
+    "pycmdtools",
 ]
 test_requires: list[str] = [
     "pylint",
